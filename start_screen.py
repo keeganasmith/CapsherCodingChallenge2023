47491b4cd15ef001d683af54e5871666ee2dc08c
keeganasmith <99233684+keeganasmith@users.noreply.github.com>
2023-03-30 02:22:35 -0500

    better start screen

--- start_screen.py
+++ start_screen.py
@@ -4,6 +4,6 @@ class start_screen:
     def __init__(self):
         self.surface = pygame.image.load("start_screen_background.png")
         self.loc = [0,0]
-        self.start_button = pygame.image.load("Start button.png")
-        self.start_button_loc = [600, 350]
+        self.start_button = pygame.image.load("Start_button.png")
+        self.start_button_loc = [500, 250]
         self.surface.blit(self.start_button, (self.start_button_loc))
